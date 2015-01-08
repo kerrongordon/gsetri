306310b00ee8a3a7262e3b80bef7626e258b6536
kerrongordon <kgpsounds.com@gmail.com>
2015-01-08 19:18:57 -0400

    remove page animation

--- app/js/main.js
+++ app/js/main.js
@@ -3,22 +3,6 @@ new WOW().init();
 
 $(document).ready(function() {
 
-	$('.pageview').css('margin-top', '0');
-
-	$('a').click(function() {
-
-		event.preventDefault();
-
-		newLocation = this.href;
-
-		$('.pageview').css('margin-top', '100vh').delay(50).fadeOut( 50, newpage );
-
-	});
-
-	function newpage() {
-
-		window.location = newLocation;
-
-	}
+	
 
 });
