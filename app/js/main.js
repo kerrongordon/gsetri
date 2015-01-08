5f9e248120e212dfa79045380f687487d2eb7942
kerrongordon <kgpsounds.com@gmail.com>
2015-01-08 19:15:38 -0400

    update

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,24 @@
 new WOW().init();
 
+
+$(document).ready(function() {
+
+	$('.pageview').css('margin-top', '0');
+
+	$('a').click(function() {
+
+		event.preventDefault();
+
+		newLocation = this.href;
+
+		$('.pageview').css('margin-top', '100vh').delay(50).fadeOut( 50, newpage );
+
+	});
+
+	function newpage() {
+
+		window.location = newLocation;
+
+	}
+
+});
