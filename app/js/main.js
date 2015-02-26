71425a76d6d31d33e1f1b29479dcca2c911e07d8
kerrongordon <kgpsounds.com@gmail.com>
2015-02-26 18:24:39 -0400

    update design

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,19 @@
-new WOW().init();
-
-
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	$('.navbar').css("margin", "0px");
-	$(window).bind('scroll', function() {
-	    if ($(window).scrollTop() > 250 ) {
-	        $('.navbar').addClass('gototop animated fadeInDown');
-	     }
-	    else {
-	        $('.navbar').removeClass('gototop animated fadeInDown');
-	    }
-	});
-} else {
+'use strict';
 
 
 
-}
+jQuery(document).ready(function($){
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		$('.navbar').css('margin', '0px');
+		$(window).bind('scroll', function() {
+		    if ($(window).scrollTop() > 250 ) {
+		        $('.navbar').addClass('gototop animated fadeInDown');
+		     }
+		    else {
+		        $('.navbar').removeClass('gototop animated fadeInDown');
+		    }
+		});
+	} else {
+
+	}
+});
