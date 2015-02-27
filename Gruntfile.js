f43a52c9817e9938660f045b5f57c1503b9c8074
kerrongordon <kgpsounds.com@gmail.com>
2015-02-27 11:18:52 -0400

    remove fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -250,9 +250,10 @@ module.exports = function (grunt) {
             'img/**/*',
             'fonts/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore.
-            '!**/_*{,/**}'
+            '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
             //'_bower_components/jquery/jquery.js',
+            '_bower_components/fontawesome/fonts/*'
             //'favicon.ico',
             //'apple-touch*.png'
           ],
