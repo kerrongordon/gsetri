910a56808360068ec44ec5c6c0334e2d3ca31c54
kerrongordon <kgpsounds.com@gmail.com>
2015-03-03 19:01:35 -0400

    update

--- app/js/main.js
+++ app/js/main.js
@@ -7,16 +7,21 @@ jQuery(document).ready(function($){
 		
 		$('.navbar').css('margin', '0px');
 		$(window).bind('scroll', function() {
-		    if ($(window).scrollTop() > 250 ) {
-		        $('.navbar').addClass('gototop animated fadeInDown');
+		    if ($(window).scrollTop() > $('.hero-bg').height() ) {
+		        $('#navbar').addClass('gototop animated fadeInDown');
 		     }
 		    else {
-		        $('.navbar').removeClass('gototop animated fadeInDown');
+		        $('#navbar').removeClass('gototop animated fadeInDown');
 		    }
 		});
-
+		$('.page-head-bg').css({'top': '-70%' });
 	} else {
-
+		$(window).scroll(function() {
+		  var windowScroll = $(this).scrollTop()/1.5;
+		  $('.page-head-bg').css({
+		    'top': '-' + windowScroll + 'px'
+		  });
+		});
 	}
     $('#Contact-hide').hide();
 });
